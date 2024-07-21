465bfb480f2bbc22698b3fa6b5bc54c4b3edeb67
imatishkumarjaiwal <imatishkumarjaiwal@gmail.com>
2024-07-21 10:58:50 +0530

    Initial upload of CRM

---
+++ database/migrations/2024_07_19_034724_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id(); // Auto-incrementing primary key.
+            $table->string('username'); // Username column.
+            $table->string('password'); // Password column.
+            $table->unsignedBigInteger('admin_id')->nullable(); // Foreign key to admin.
+            $table->unsignedBigInteger('client_id')->nullable(); // Foreign key to client.
+            $table->unsignedBigInteger('staff_id')->nullable(); // Foreign key to staff.
+            $table->unsignedBigInteger('created_by')->nullable(); // Foreign key for created by.
+            $table->timestamp('created_on')->nullable(); // Timestamp for creation.
+            $table->unsignedBigInteger('updated_by')->nullable(); // Foreign key for updated by.
+            $table->timestamp('updated_on')->nullable(); // Timestamp for update.
+            $table->unsignedBigInteger('deleted_by')->nullable(); // Foreign key for deleted by.
+            $table->timestamp('deleted_on')->nullable(); // Timestamp for deletion.
+            $table->boolean('deleted_status')->default(false); // Boolean status for deletion.
+            $table->timestamp('last_updated')->nullable(); // Timestamp for last updated.
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
